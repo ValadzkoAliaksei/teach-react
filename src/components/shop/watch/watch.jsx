781e6ce57dd2f33774bb6bd408882d1b2ac4a908
valadzkoaliaksei <valadzkoaliaksei@tut.by>
2022-01-20 09:48:14 +0300

    feat: перевел на хуки

--- src/components/shop/watch/watch.jsx
+++ src/components/shop/watch/watch.jsx
@@ -1,53 +1,40 @@
+import { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 
-export class Watch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      seconds: 30,
-    };
-  }
+export const Watch = ({ clearState }) => {
+  const [seconds, setSeconds] = useState(10);
+  const intervalRef = useRef();
 
-  componentDidMount() {
-    this.timerId = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerId);
-  }
-
-  tick() {
-    const { seconds } = this.state;
-    const { clearState } = this.props;
-    if (seconds !== 1) {
-      this.setState((prevState) => ({
-        seconds: prevState.seconds - 1,
-      }));
-    } else {
-      this.setState((prevState) => ({
-        seconds: prevState.seconds - 1,
-      }));
-      clearState();
-      clearInterval(this.timerId);
-    }
-  }
-
-  formatTime(num) {
+  const formatTime = (num) => {
     if (num < 10) return `0${num}`;
     return num;
-  }
+  };
 
-  render() {
-    const { seconds } = this.state;
-    return (
-      <div>
-        <span>{this.formatTime(Math.floor(seconds / 60))}:</span>
-        <span>{this.formatTime(seconds % 60)}</span>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    if (seconds === 0) {
+      clearState();
+      clearInterval(intervalRef.current);
+    }
+    clearState();
+  }, [seconds, clearState]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+    intervalRef.current = timerId;
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <div>
+      <span>{formatTime(Math.floor(seconds / 60))}:</span>
+      <span>{formatTime(seconds % 60)}</span>
+    </div>
+  );
+};
 
 Watch.propTypes = {
   clearState: PropTypes.func.isRequired,
