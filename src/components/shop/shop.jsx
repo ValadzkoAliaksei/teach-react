c4a9b44d6769263c9fd80333de6b705885f457b6
Aliaksei Valadzko <61273546+ValadzkoAliaksei@users.noreply.github.com>
2022-02-17 17:17:04 +0300

    Revert "feat: добавил thunk"

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,11 +1,9 @@
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 
 import { Cart } from './cart';
 import { Product } from './product';
 
 import { buyProducts, clearProducts } from '../../store/products-state';
-import { getStorage } from '../../store/storage-state/thunk';
 
 import { products } from '../../constants/products';
 
@@ -21,10 +19,6 @@ export const Shop = () => {
   const clearState = () => {
     dispatch(clearProducts());
   };
-
-  useEffect(() => {
-    dispatch(getStorage());
-  }, []);
 
   return (
     <div className={style.shop}>
